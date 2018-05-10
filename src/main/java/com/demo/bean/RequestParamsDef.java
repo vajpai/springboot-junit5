4e4165b4ad026a4fca150ff61843ca62134ca935
Vajpai Bingi <vajpai.bingi@lowes.com>
2018-05-10 18:19:30 +0530

    cleanup code

--- src/main/java/com/demo/bean/RequestParamsDef.java
+++ src/main/java/com/demo/bean/RequestParamsDef.java
@@ -10,21 +10,5 @@ import lombok.ToString;
 public class RequestParamsDef {
   private String keywords;
   private String departments;
-  private Integer storeNumber;
-  private long contractId = 0;
-  private boolean isEmployee = false;
-  private boolean showRelatedCats = false;
-  private boolean showJumpNav = false;
-  private boolean showVariants = false;
-  private boolean isMobile = false;
-  private boolean showURL = false;
-  private boolean rollUpVariants = false;
-  private String sortMethod;
-  private String searchSegment;
-  private String searchSegmentList;
-  private Integer offset;
-  private Integer maxResults;
-  private String channelType;
-  private String site;
 
 }
